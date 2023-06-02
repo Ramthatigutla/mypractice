832632346f20738ebe8d87e5771ce447bfb93b25
ram <ramthatigutla@gmail.com>
2023-06-03 01:26:41 +0530

    first commit

---
+++ 01_shell.sh
@@ -0,0 +1,33 @@
+#This is my first own shell script
+************************************
+Author     : Ram
+Created on : 03-06-2023
+Description: Basic Linux commands
+************************************
+
+echo "Current working directory"
+pwd
+
+echo "list of files and directories"
+
+ls
+
+echo "todays date is:"
+date
+
+echo "creating a new directory:"
+mkdir firstone
+
+echo "creating a new file"
+
+vim first.txt
+
+echo "looking into the content"
+
+cat first.txt
+
+echo "removing directory"
+rm -rf firstone
+
+
+
